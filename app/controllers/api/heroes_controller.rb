ada2699850ff8a964e776378086ff3b526801827
raphael-souza <raphael3130@gmail.com>
2020-02-08 11:56:10 -0300

    request in front Angular ok

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -5,13 +5,44 @@ module Api
     def index 
         heroes = Hero.order('created_at DESC')
         
-        render json: {status: 'SUCCESS', message: 'heroes carregados', data: heroes}, status: :ok
+        render json: { status: 'SUCCESS', message: 'heroes carregados', data: heroes }, status: :ok
     end
     
     def show
       hero = Hero.find(params[:id])
 
-      render json: {status: 'SUCCESS', message: 'hero carregado', data: hero}, status: :ok
+      render json: { status: 'SUCCESS', message: 'hero carregado', data: hero }, status: :ok
+    end
+
+    def create
+      hero = Hero.new(hero_params)
+      if hero.save()
+        render json: { status: 'SUCCESS', message: 'novo hero cadastrado', data: hero }, status: :ok
+      else
+        render json: { status: 'ERROR', message:'Hero não foi salvo', data: hero.errors }, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      hero = Hero.find(params[:id])
+      if  hero.update(name: params[:name])
+        render json: { status: 'SUCCESS', message: 'novo hero atualizado', data: hero }, status: :ok
+      else
+        render json: { status: 'ERROR', message:'Hero não foi atualizado', data: hero.errors }, status: :unprocessable_entity
+      end
+    end
+
+
+    def destroy
+      hero = Hero.find(params[:id])
+      hero.destroy
+
+      render json: { status: 'SUCCESS', message: 'Hero deletado', data: hero }, status: :ok
+    end
+
+    private 
+    def hero_params
+      params.permit(:name)
     end
   end
 end
